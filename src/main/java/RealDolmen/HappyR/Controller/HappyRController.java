e8c2f854e5a7391eb89346c055a97c0c231c9bb8
Wieland Vandebotermet <wieland.vandebotermet@inetum-realdolmen.world>
2024-03-13 16:14:57 +0100

    update

--- src/main/java/RealDolmen/HappyR/Controller/HappyRController.java
+++ src/main/java/RealDolmen/HappyR/Controller/HappyRController.java
@@ -1,4 +1,20 @@
 package RealDolmen.HappyR.Controller;
 
+import RealDolmen.HappyR.Service.UserService;
+import RealDolmen.HappyR.model.User;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class HappyRController {
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(UserService userService) {
+        return userService.getUserList();
+    }
 }
